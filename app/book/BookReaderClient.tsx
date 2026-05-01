521c8e5ac56f89e82ca307ef8e7bf48be9fa654a
atsunazo <atsunazo0217@gmail.com>
2026-05-01 14:50:07 +0900

    V11.2

--- app/book/BookReaderClient.tsx
+++ app/book/BookReaderClient.tsx
@@ -631,7 +631,7 @@ export default function BookReaderClient() {
     window.setTimeout(() => {
       suppressScrollSync.current = false;
       syncIndexFromScroll();
-    }, behavior === "smooth" ? 200 : 40);
+    }, behavior === "smooth" ? 200 : 60);
   }
 
   function goPrev() {
